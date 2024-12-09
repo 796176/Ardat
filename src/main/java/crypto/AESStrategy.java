b8e66105c2a381f36b521d34f217240d97ccdf69
yav <796176@protonmail.com>
2024-12-10 00:38:04 +0300

    Comment AESStrategy

--- src/main/java/crypto/AESStrategy.java
+++ src/main/java/crypto/AESStrategy.java
@@ -21,9 +21,26 @@ package crypto;
 import java.nio.ByteBuffer;
 import java.security.GeneralSecurityException;
 
+/**
+ * AESStrategy is an interface that provides a straightforward way for AES encryption and decryption. It allows
+ * concrete implementations to choose the specific block cipher mode e.g. CTR, CBC. The barebone implementation
+ * ( without extending the public interface ) implies that the padding and de-padding is left to the client.
+ */
 public interface AESStrategy {
 
+	/**
+	 * Encrypts the passed data.
+	 * @param input the data
+	 * @return the encrypted data
+	 * @throws GeneralSecurityException if exceptions related to javax.crypto.Cipher occurred
+	 */
 	ByteBuffer encrypt(ByteBuffer input) throws GeneralSecurityException;
 
+	/**
+	 * Decrypts the encrypted data.
+	 * @param input the encrypted data
+	 * @return the decrypted data
+	 * @throws GeneralSecurityException if exceptions related to javax.crypto.Cipher occurred
+	 */
 	ByteBuffer decrypt(ByteBuffer input) throws GeneralSecurityException;
 }
